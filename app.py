169ddc888f0f3f655861814e6a3788aafe17a415
[InaLuo2022] <117559158+InaLuo2022@users.noreply.github.com>
2023-02-09 21:11:50 +1100

    update

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ Base.prepare(engine, reflect = True)
 Base.classes.keys()
 
 # Save references to each table
-# Assign the 'measurement' to a variable called 'Measurement'
 measurement = Base.classes.measurement
 station_list = Base.classes.station
 
@@ -30,14 +29,10 @@ station_list = Base.classes.station
 # Create a session
 session = Session(engine)
 
-#################################################
 # Flask Setup
-#################################################
 app = Flask(__name__)
 
-#################################################
 # Flask Routes
-#################################################
 # "/" Start at the homepage. List all the available routes.
 
 @app.route("/")
@@ -54,15 +49,19 @@ def welcome():
         f'input date in the format of YYYY-MM-DD')
 
 # Convert the query results to a dictionary by using date as the key and prcp as the value.
-# Return the JSON representation of your dictionary.
+# Return the JSON representation of the dictionary.
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Query all passengers
-    results = session.query(measurement.date,measurement.prcp).all()
+    # Query date and prcp
+    result_date = session.query(measurement.date).all()
+    result_prcp = session.query(measurement.prcp).all()
 
-    results_list = list(np.ravel(results))
+    result_date_list = list(np.ravel(result_date))
+    result_prcp_list = list(np.ravel(result_prcp))
 
-    return jsonify(results_list)
+    date_prcp_list = dict(zip(result_date_list, result_prcp_list))
+
+    return jsonify(date_prcp_list)
 
 # Return a JSON list of stations from the dataset.
 @app.route("/api/v1.0/stations")
@@ -75,10 +74,10 @@ def stations():
 
     return jsonify(stations_all)
 
+# list date, tobs for the most active station (USC00519281) 
 @app.route("/api/v1.0/tobs")
 def tobs():
 
-    # list date, tobs for  the most active station (USC00519281) 
     date_USC00519281 = session.query(measurement.date).filter(measurement.station == 'USC00519281', \
         measurement.date <= '2017-08-23', measurement.date >= '2016-08-23').all()
 
@@ -92,20 +91,20 @@ def tobs():
 
     return jsonify(USC00519281_list)
 
+# Returns the min, max, and average temperatures calculated from the given start date to the end of the dataset
 @app.route("/api/v1.0/<start_date>")
 def start_route(start_date):
 
-    # Returns the min, max, and average temperatures calculated from the given start date to the end of the dataset
     tobs_min = session.query(func.min(measurement.tobs)).filter(measurement.date >= start_date).all()
     tobs_max = session.query(func.max(measurement.tobs)).filter(measurement.date >= start_date).all()
     tobs_avg = session.query(func.avg(measurement.tobs)).filter(measurement.date >= start_date).all()
 
     return jsonify({'min_temperature': tobs_min},{'max_temperature':tobs_max},{'avg_temperature':tobs_avg})
 
+# Returns the min, max, and average temperatures calculated from the given start date to the end of the dataset
 @app.route("/api/v1.0/<start_date>/<end_date>")
 def start_end_route(start_date,end_date):
 
-    # Returns the min, max, and average temperatures calculated from the given start date to the end of the dataset
     tobs_min_1 = session.query(func.min(measurement.tobs)).filter(measurement.date >= start_date, measurement.date <= end_date).all()
     tobs_max_1 = session.query(func.max(measurement.tobs)).filter(measurement.date >= start_date, measurement.date <= end_date).all()
     tobs_avg_1 = session.query(func.avg(measurement.tobs)).filter(measurement.date >= start_date, measurement.date <= end_date).all()
